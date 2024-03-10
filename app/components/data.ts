2ce9a4fe43d6cd6a93b78a32b7357b8ad16bc8e1
koya <koya9431ptr@gmail.com>
2024-03-10 01:00:04 +0000

    close switter

--- app/components/data.ts
+++ app/components/data.ts
@@ -9,16 +9,16 @@ export const projectData = [
         gitHubLink: 'https://github.com/koya-tech/jatflix',
         demoUrl: 'https://jatflix-e6701.web.app/'
     },
+    // {
+    //     id: 2,
+    //     title: 'Switter',
+    //     description:
+    //         'This is Space themed basic CRUD(Create, Read, Update, Delete) application. ',
+    //     gitHubLink: 'https://github.com/koya-tech/thread-ky',
+    //     demoUrl: 'https://thread-ky.vercel.app/'
+    // },
     {
         id: 2,
-        title: 'Switter',
-        description:
-            'This is Space themed basic CRUD(Create, Read, Update, Delete) application. ',
-        gitHubLink: 'https://github.com/koya-tech/thread-ky',
-        demoUrl: 'https://thread-ky.vercel.app/'
-    },
-    {
-        id: 3,
         title: 'Coming soon ...',
         description:
             'I am in the process of making it',
